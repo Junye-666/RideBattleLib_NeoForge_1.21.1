1a4e351ee3ca9f845279bc7d3ae520eccef53993
Junye <d_d5du@163.com>
2025-05-05 20:59:28 +0200

    基础架构,变身,解除系统

--- src/main/java/com/jpigeon/ridebattlelib/system/rider/basic/condition/HenshinCondition.java
+++
@@ -1,8 +0,0 @@
-package com.jpigeon.ridebattlelib.system.rider.basic.condition;
-
-import com.jpigeon.ridebattlelib.system.rider.basic.RiderConfig;
-import net.minecraft.world.entity.player.Player;
-
-public interface HenshinCondition {
-    boolean check(Player player, RiderConfig config);
-}

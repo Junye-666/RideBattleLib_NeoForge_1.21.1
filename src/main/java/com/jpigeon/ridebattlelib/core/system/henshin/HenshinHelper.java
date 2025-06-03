749d6a3575336d812517a79fd4ed009bfc9c6f7a
Junye <d_d5du@163.com>
2025-06-03 20:03:38 +0200

    小优化

--- src/main/java/com/jpigeon/ridebattlelib/core/system/henshin/HenshinHelper.java
+++ src/main/java/com/jpigeon/ridebattlelib/core/system/henshin/HenshinHelper.java
@@ -17,7 +17,6 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
 import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;

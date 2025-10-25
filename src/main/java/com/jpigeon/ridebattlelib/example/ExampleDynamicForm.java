4eef6ce9688052605303de8d575aaab35ab32c98
Junye <d_d5du@163.com>
2025-10-25 17:17:05 +0200

    DynamicFormConfig

--- src/main/java/com/jpigeon/ridebattlelib/example/ExampleDynamicForm.java
+++ src/main/java/com/jpigeon/ridebattlelib/example/ExampleDynamicForm.java
@@ -7,7 +7,6 @@ import com.jpigeon.ridebattlelib.core.system.henshin.RiderConfig;
 import com.jpigeon.ridebattlelib.core.system.henshin.RiderRegistry;
 import com.jpigeon.ridebattlelib.core.system.henshin.helper.TriggerType;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -35,14 +34,13 @@ public class ExampleDynamicForm {
                     BETA_SLOT_1,
                     List.of(Items.EMERALD, Items.DIAMOND),
                     true,
-                    false)
+                    true)
             .addDriverSlot(
                     BETA_SLOT_2,
                     List.of(Items.REDSTONE, Items.GLOWSTONE_DUST),
                     true,
-                    false)
-            .setAllowDynamicForms(true)
-            .setCommonArmor(EquipmentSlot.FEET, Items.LEATHER_BOOTS);
+                    true)
+            .setAllowDynamicForms(true);
 
     public static final FormConfig baseForm = new FormConfig(BETA_BASE_FORM)
             .setTriggerType(TriggerType.KEY)
@@ -56,8 +54,7 @@ public class ExampleDynamicForm {
             .addAttribute(
                     ResourceLocation.fromNamespaceAndPath("minecraft", "generic.max_health"),
                     8.0,
-                    AttributeModifier.Operation.ADD_VALUE)
-            ;
+                    AttributeModifier.Operation.ADD_VALUE);
 
 
     public static void betaRider() {
@@ -74,24 +71,24 @@ public class ExampleDynamicForm {
 
     private static void registerDynamicMappings() {
         // 钻石 -> 钻石头盔 + 跳跃提升 + 伤害吸收
-        DynamicFormConfig.registerItemArmor(Items.DIAMOND, Items.DIAMOND_HELMET);
-        DynamicFormConfig.registerItemEffects(Items.DIAMOND, (MobEffectInstance) MobEffects.JUMP);
-        DynamicFormConfig.registerItemEffects(Items.DIAMOND, (MobEffectInstance) MobEffects.ABSORPTION);
+        DynamicFormConfig.registerItemArmor(Items.DIAMOND, EquipmentSlot.HEAD, Items.DIAMOND_HELMET);
+        DynamicFormConfig.registerItemEffect(Items.DIAMOND, MobEffects.JUMP);
+        DynamicFormConfig.registerItemEffect(Items.DIAMOND, MobEffects.ABSORPTION);
         DynamicFormConfig.registerItemGrantedItems(Items.DIAMOND, new ItemStack(Items.DIAMOND_AXE)
         );
 
         // 绿宝石 -> 龟头 + 抗性效果
         DynamicFormConfig.registerItemArmor(Items.EMERALD, Items.TURTLE_HELMET);
-        DynamicFormConfig.registerItemEffects(Items.EMERALD, (MobEffectInstance) MobEffects.DAMAGE_RESISTANCE);
+        DynamicFormConfig.registerItemEffect(Items.EMERALD, MobEffects.DAMAGE_RESISTANCE);
         DynamicFormConfig.registerItemGrantedItems(Items.EMERALD, new ItemStack(Items.GOLDEN_CARROT));
 
         // 红石 -> 铁胸甲 + 伤害提升
         DynamicFormConfig.registerItemArmor(Items.REDSTONE, Items.IRON_CHESTPLATE);
-        DynamicFormConfig.registerItemEffects(Items.REDSTONE, (MobEffectInstance) MobEffects.DAMAGE_BOOST);
+        DynamicFormConfig.registerItemEffect(Items.REDSTONE, MobEffects.DAMAGE_BOOST);
 
         // 萤石粉 -> 金甲 + 速度效果
         DynamicFormConfig.registerItemArmor(Items.GLOWSTONE_DUST, Items.GOLDEN_CHESTPLATE);
-        DynamicFormConfig.registerItemEffects(Items.GLOWSTONE_DUST, (MobEffectInstance) MobEffects.MOVEMENT_SPEED);
+        DynamicFormConfig.registerItemEffect(Items.GLOWSTONE_DUST, MobEffects.MOVEMENT_SPEED);
     }
 
     public static void init() {

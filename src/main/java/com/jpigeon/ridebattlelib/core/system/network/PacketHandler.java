9b31648fef4bf03e3e1193bc4b7b3da259c64bcc
Junye <d_d5du@163.com>
2026-02-14 13:09:37 +0100

    1.0.7

--- src/main/java/com/jpigeon/ridebattlelib/core/system/network/PacketHandler.java
+++ src/main/java/com/jpigeon/ridebattlelib/core/system/network/PacketHandler.java
@@ -22,7 +22,7 @@ import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 public class PacketHandler {
     public static void register(final RegisterPayloadHandlersEvent event) {
         event.registrar(RideBattleLib.MODID)
-                .versioned("1.0.5")
+                .versioned("1.0.7")
                 .playToServer(DriverActionPacket.TYPE, DriverActionPacket.STREAM_CODEC,
                         (payload, context) -> {
                             Player targetPlayer = context.player().level().getPlayerByUUID(payload.playerId());

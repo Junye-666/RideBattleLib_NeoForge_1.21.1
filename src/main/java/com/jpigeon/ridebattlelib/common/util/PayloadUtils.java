b5dc64c340e31d3b3670c2c1681522528936c87e
Junye <d_d5du@163.com>
2026-03-30 18:08:09 +0200

    1.2.1

--- src/main/java/com/jpigeon/ridebattlelib/common/util/PayloadUtils.java
+++ src/main/java/com/jpigeon/ridebattlelib/common/util/PayloadUtils.java
@@ -4,6 +4,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class PayloadUtils {
     /**
